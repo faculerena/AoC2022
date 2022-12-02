e576310d6ca35c97c03fcae8ae685842c0ebe6e6
Facundo Lerena <faculerena@gmail.com>
2022-12-02 11:41:14 -0300

    Day 2

--- AoC_02/main.go
+++ AoC_02/main.go
@@ -9,15 +9,15 @@ import (
 
 func main() {
 	dict := map[string]int{
-		"A X": 1 + 1 + 3,
-		"A Y": 1 + 2,
-		"A Z": 1 + 3 + 6,
-		"B X": 2 + 1 + 6,
-		"B Y": 2 + 2 + 3,
-		"B Z": 2 + 3,
-		"C X": 3 + 1,
-		"C Y": 3 + 2 + 6,
-		"C Z": 3 + 3 + 3,
+		"A X": 1 + 3,
+		"A Y": 1,
+		"A Z": 1 + 6,
+		"B X": 2 + 6,
+		"B Y": 2 + 3,
+		"B Z": 2,
+		"C X": 3,
+		"C Y": 3 + 6,
+		"C Z": 3 + 3,
 	}
 
 	input, err := os.ReadFile("input.txt")
