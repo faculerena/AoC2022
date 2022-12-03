6a0dcb786fcf626784068386e927d2d7d99e67df
Facundo Lerena <faculerena@gmail.com>
2022-12-03 00:25:31 -0300

    Refactor Day 2 - no updates

--- AoC_02/main.go
+++ AoC_02/main.go
@@ -8,42 +8,17 @@ import (
 )
 
 func main() {
-	dict := map[string]int{
-		"A X": 1 + 3,
-		"A Y": 2 + 6,
-		"A Z": 3,
-		"B X": 1,
-		"B Y": 2 + 3,
-		"B Z": 3 + 6,
-		"C X": 1 + 6,
-		"C Y": 2,
-		"C Z": 3 + 3,
-	}
-	dict2 := map[string]int{
-		"A X": 3,
-		"A Y": 1 + 3,
-		"A Z": 2 + 6,
-		"B X": 1,
-		"B Y": 2 + 3,
-		"B Z": 3 + 6,
-		"C X": 2,
-		"C Y": 3 + 3,
-		"C Z": 1 + 6,
-	}
-
+	dict := map[string]int{"A X": 4, "A Y": 8, "A Z": 3, "B X": 1, "B Y": 5, "B Z": 9, "C X": 7, "C Y": 2, "C Z": 6}
+	dict2 := map[string]int{"A X": 3, "A Y": 4, "A Z": 8, "B X": 1, "B Y": 5, "B Z": 9, "C X": 2, "C Y": 6, "C Z": 7}
 	input, err := os.ReadFile("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	games, score1, score2 := strings.Split(string(input), "\n"), 0, 0
 	for i := range games {
-		value_part1, _ := dict[games[i]]
-		value_part2, _ := dict2[games[i]]
+		value_part1, value_part2 := dict[games[i]], dict2[games[i]]
 		score1 += value_part1
 		score2 += value_part2
 	}
-	fmt.Println(fmt.Sprint(score1))
-	fmt.Println(fmt.Sprint(score2))
-
+	fmt.Println(fmt.Sprint(score1) + ", " + fmt.Sprint(score2))
 }
